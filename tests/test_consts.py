29a4504fcd801ac3a70f6b6da4e326657a25bf27
alex <xfanth@gmail.com>
2025-03-12 19:30:32 +0000

    force CICD

---
+++ tests/test_consts.py
@@ -0,0 +1,14 @@
+# content of test_some_are_slow.py
+import time
+
+
+def test_funcfast():
+    time.sleep(0.1)
+
+
+def test_funcslow1():
+    time.sleep(0.2)
+
+
+def test_funcslow2():
+    time.sleep(0.3)
