b4b421cf02760e2fc0d98d142de8f1b124d23031
alex <xfanth@gmail.com>
2025-03-16 20:08:19 +0000

    remove file

--- providers.py
+++
@@ -1,6 +0,0 @@
-cooldowns = {}
-cooldowns = {base_url: CooldownProvider(cooldown_seconds) for model in models}
-
-def set_cooldown(base_url):
-    if base_url not in cooldowns:
-        cooldowns[base_url] = CooldownProvider(cooldown_seconds)
